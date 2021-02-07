5a0220947c92472023ca80acd6b1d30631117033
Batuhan Göksu <batuhangoksu@gmail.com>
2021-02-07 20:34:16 +0300

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,10 +1,12 @@
 package handler
 
 import (
-  "fmt"
-  "net/http"
+    "fmt"
+    "net/http"
+    "time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+    currentTime := time.Now().Format(time.RFC850)
+    fmt.Fprintf(w, currentTime)
 }
